db38559fb2746962ba8130e45050ea515015b554
Weelawawt Sampao <Weelewat@gmail.com>
2022-04-06 09:08:16 +0700

    clone part 5

--- Clone project/mysite/blog/models.py
+++ Clone project/mysite/blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
-    published_date = models.DateTimeField(blank=True, null=Ture)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.noew()
--- Clone project/mysite/blog/urls.py
+++ Clone project/mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from blog import views
+from mysite.blog.views import comment_approve
 
 urlpatterns = [
     path('/', views.PostListView.as_view(), name='post_list'),
@@ -9,4 +10,8 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_remove'),
     path('draft/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish')
 ]
